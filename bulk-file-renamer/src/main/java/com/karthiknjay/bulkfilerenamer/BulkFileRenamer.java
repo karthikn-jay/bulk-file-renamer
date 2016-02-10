84fdf041c892d5a4319bf341f7af005ab3cb825b
Karthikeyan Jayachandran <karthik.jm85@gmail.com>
2016-02-10 23:51:20 +0530

    Basic UI works all done

--- bulk-file-renamer/src/main/java/com/karthiknjay/bulkfilerenamer/BulkFileRenamer.java
+++ bulk-file-renamer/src/main/java/com/karthiknjay/bulkfilerenamer/BulkFileRenamer.java
@@ -2,25 +2,26 @@ package com.karthiknjay.bulkfilerenamer;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
+import com.karthiknjay.bulkfilerenamer.util.Constants;
 import com.karthiknjay.bulkfilerenamer.util.SwingUtils;
 
 public class BulkFileRenamer {
@@ -30,23 +31,26 @@ public class BulkFileRenamer {
 		gui.showGUI();
 	}
 	
-	private static String VERSION = "1.0.0.0";
+	private static String VERSION = "1.0.0";
 
 	private JFrame jf;
+	
 	private JPanel pnlHeader;
 	private JComponent pnlCenter;
 	private JPanel pnlFooter;
-	private JLabel lblStatus;
-	private JButton btnDownload;
-	private JTextField txtSaveFolder;
-	private JTable table;
-	private JButton btnTestConnection;
-	private JButton btnErpDownload;
 	
-	private static final String LBL_FILES_NOT_FOUND = "No File(s) Found.";
-	private static final String LBL_DOWNLOAD_INFO = "Click Upload to upload the file(s).";
-	private static final String LBL_RETRIEVING_FILES = "Retrieving files. Please wait...";
-	private static final String LBL_UPLOAD_SUCCESS = "File(s) added to the Upload scheduler.";
+	private JTextField txtBrowseFolder;
+	private JButton btnBrowse;
+	private JFileChooser fcBrowse;
+	
+	private JTextField txtFind;
+	private JTextField txtReplace;
+	private JButton btnFind;
+	private JButton btnReplace;
+	
+	private JTextArea txtResult;
+	
+	private JLabel lblStatus;
 	
 	private void showGUI() {
 		// Set client OS's look and feel
@@ -69,20 +73,19 @@ public class BulkFileRenamer {
 	
 	private void buildGUI() {
 		try {
-			jf = new JFrame("FrameDemo");
+			jf = new JFrame(Constants.APP_TITLE);
 			jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			jf.getContentPane().setLayout(new BorderLayout()); // set layout
 			
-			// add all panels to main applet container
 			jf.getContentPane().add(getHeaderPanel(), BorderLayout.NORTH);
 			
-			pnlCenter = getCenterPanel(new ArrayList());
+			pnlCenter = getCenterPanel();
 			jf.getContentPane().add(pnlCenter, BorderLayout.CENTER);
 			
 			jf.getContentPane().add(getFooterPanel(), BorderLayout.SOUTH);
 			
 			jf.pack();
-			jf.setSize(600, 450);
+			jf.setSize(750, 450);
 			jf.setLocation(200, 200);
 			jf.setVisible(true);
 			
@@ -94,68 +97,75 @@ public class BulkFileRenamer {
 	
 	private Component getHeaderPanel() {
 		pnlHeader = new JPanel();
-		//headerPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
 		pnlHeader.setLayout(new BoxLayout(pnlHeader, BoxLayout.LINE_AXIS));
 		
-		txtSaveFolder = new JTextField();
-		txtSaveFolder.setText("");
+		txtBrowseFolder = new JTextField("", 30);
+		txtBrowseFolder.setEditable(false);
 		
-		JPanel pnlFilePath = new JPanel();
-		pnlFilePath.setLayout(new FlowLayout(FlowLayout.LEADING));
-		pnlFilePath.add(new JLabel("ERP Config folder : " ));
+		btnBrowse = new JButton("Browse...");
+		btnBrowse.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				fcBrowse = new JFileChooser();
+				fcBrowse.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int fcStatus = fcBrowse.showOpenDialog(jf);
+				
+				if( fcStatus == JFileChooser.APPROVE_OPTION) {
+					String selectedFolder = fcBrowse.getSelectedFile().getAbsolutePath();
+					txtBrowseFolder.setText(selectedFolder);
+					System.out.println("selectedFolder : " + selectedFolder);
+				}
+			}
+			
+		});
 		
-		JTextField txtFilePath = new JTextField( "", 30 );
-		txtFilePath.setEditable(false);
-		pnlFilePath.add(txtFilePath);
+		JPanel hPanel1 = new JPanel();
+		hPanel1.setLayout(new FlowLayout(FlowLayout.LEADING));
+		hPanel1.add(new JLabel(Constants.LBL_BROWSE));
 		
-		pnlHeader.add(pnlFilePath);
-		pnlHeader.add(Box.createHorizontalGlue());
-		pnlHeader.add(new JLabel("v" + VERSION));
+		hPanel1.add(txtBrowseFolder);
+		hPanel1.add(btnBrowse);
 		
+		txtFind = new JTextField("", 15);
+		txtReplace = new JTextField("", 15);
+		btnFind = new JButton("Find");
+		btnReplace = new JButton("Replace");
+		
+		JPanel hPanel2 = new JPanel();
+		hPanel2.setLayout(new FlowLayout(FlowLayout.LEADING));
+		
+		hPanel2.add(new JLabel("Find: "));
+		hPanel2.add(txtFind);
+		hPanel2.add(new JLabel("Replace: "));
+		hPanel2.add(txtReplace);
+		hPanel2.add(btnFind);
+		hPanel2.add(btnReplace);
+		
+		JPanel hPanel = new JPanel();
+		hPanel.setLayout(new GridLayout(2, 0));
+		hPanel.add(hPanel1);
+		hPanel.add(hPanel2);
+		
+		pnlHeader.add(hPanel);
 		return pnlHeader;
 	}
 	
-	public JComponent getCenterPanel(List filesList) {
-		JPanel panel = new JPanel();
-		if(filesList == null || filesList.size() == 0) {
-			panel.add(new JLabel("No File(s) found."), BorderLayout.CENTER);
-			panel.revalidate();
-			panel.repaint();
-			return panel;
-		} else {
-			
-			/*tableModel = new FilesListTableModel();
-			tableModel.setModel(filesList);*/
-			
-			table = new JTable();
-	        table.setPreferredScrollableViewportSize(new Dimension(600,200));
-			table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-			table.setAutoResizeMode(JTable.WIDTH);
-			
-			final JScrollPane scrollPane = new JScrollPane( table );
-			return scrollPane;
-		}
+	public JComponent getCenterPanel() {
+		txtResult = new JTextArea();
+		
+		final JScrollPane scrollPane = new JScrollPane( txtResult );
+		return scrollPane;
 	}
 	
 	public Component getFooterPanel() {
-		// Footer button and status panel
 		pnlFooter = new JPanel();
-		lblStatus = new JLabel(LBL_DOWNLOAD_INFO);
-		//lblStatus.setVisible(false);
-		btnDownload = new JButton("Upload");
-		
-		/*if(filesList == null || filesList.size() == 0) {
-			lblStatus.setText(LBL_FILES_NOT_FOUND);
-			btnDownload.setEnabled(false);
-		}*/
-		
-		/*btnDownload.setActionCommand(ButtonActions.ERP_IMPORT.name());
-		btnDownload.addActionListener(this);*/
+		lblStatus = new JLabel();
 		
 		pnlFooter.setLayout(new BoxLayout(pnlFooter, BoxLayout.LINE_AXIS));
 		pnlFooter.add(lblStatus);
 		pnlFooter.add(Box.createHorizontalGlue());
-		pnlFooter.add(btnDownload);
+		pnlFooter.add(new JLabel("v" + VERSION));
 		return pnlFooter;
 	}
 
---
+++ bulk-file-renamer/src/main/java/com/karthiknjay/bulkfilerenamer/util/Constants.java
@@ -0,0 +1,9 @@
+package com.karthiknjay.bulkfilerenamer.util;
+
+public class Constants {
+	
+	public static final String APP_TITLE = "Bulk File Renamer";
+	
+	public static final String LBL_BROWSE = "Select a Folder: ";
+
+}
